071b9a71b982b6cd12b71ae4c32574ce5dfa1bdf
igorv <igorvinson@gmail.com>
2023-05-02 11:48:55 -0400

    changes

---
+++ src/app/(shared)/Navbar.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+type Props = {}
+
+function Navbar({}: Props) {
+	return (
+    <header className=''>
+      <nav className=''></nav>
+    </header>
+  );
+}
+
+export default Navbar
